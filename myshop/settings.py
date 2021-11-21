9754d26ac83c948db5ee9d7b48fc9ccb45dc1695
demecode <dpk.williams@gmail.com>
2021-11-21 20:14:49 +0000

    security

--- myshop/settings.py
+++ myshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from myshop import keys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0$apzk4=h*+ge!zn@o!ko_&t(t$jz+)2+w5&t=q0chb^lf#$wa'
+SECRET_KEY = keys.prod_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
