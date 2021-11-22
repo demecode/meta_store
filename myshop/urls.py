6ec805488645d71124595554e5d5eb7828f7472e
demecode <dpk.williams@gmail.com>
2021-11-22 10:03:26 +0000

    add core url paths for store

--- myshop/urls.py
+++ myshop/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # add a core urls for the store
+    path('', include('store.urls', namespace='store'))
 ]
